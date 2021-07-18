f6b99c7f38b364c97aaa221a32a740fa5209c5e4
iamrivera <a3m3rivera@gmail.com>
2021-07-18 14:31:30 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+function findMatching(drivers, searchText){
+    return drivers.filter(driver => driver.toLowerCase() === searchText.toLowerCase());
+}
+
+// function findMatching(drivers, searchText){
+//     return drivers.filter(function(drivere){
+//         let matcher = driver.toLowerCase() === searchText.toLowerCase()
+//         return matcher
+//     })
+// }
+
+function fuzzyMatch(drivers, searchText){
+    return drivers.filter(driver => driver.split("").shift() === searchText.split("").shift())
+}
+
+// function fuzzyMatch(drivers, searchText){
+//     return drivers.filter(function(driver){
+//         let dfl = driver.split("").shift()
+//         let stfl = searchText.split("").shift()
+//         if (dfl === stfl) {return driver} 
+//     })
+// }
+
+function matchName(driversObject, searchText){
+    return driversObject.filter(function(driver){
+        let matcher = driver['name'].toLowerCase() === searchText.toLowerCase()
+        return matcher
+    })
+}
